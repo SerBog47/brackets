94270475dfa796f1baa3b39eb5f519f1c528b5c5
Sergey Bogomolov <Bogomolov15@yandex.ru>
2022-07-21 20:45:48 +0300

    update: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length%2 !== 0) return false;
+  let brackets = [];
+  let result = str
+  res = false;
+  bracketsConfig.forEach(element => brackets.push(element.join('')));
+  let i = 0;
+  while (i < 50) {
+    brackets.forEach(element2 => {
+      result = result.replace(element2, '')
+      if (result === '') return res = true
+    });
+    i++;
+  }
+  return res;
 }
